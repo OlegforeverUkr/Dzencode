75c2b643feb79797ca1dd34ef1790a9eaef9feaf
Oleg <shlyapin8848@gmail.com>
2024-09-13 14:43:19 +0300

    create models for posts and comments tune admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from posts.models import Post, Comment
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user", "title", "created_at"]
+    list_editable = ["title"]
+    search_fields = ["user__username", "title"]
+    list_filter = ["user", "created_at"]
+    fields = ["user", "title", "body"]
+    readonly_fields = ["user"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "post", "created_at"]
+    search_fields = ["user__username", "post__title"]
+    list_filter = ["user", "post", "created_at"]
+    fields = [("post", "user"), "body", "image", "file"]
+    readonly_fields = ["post", "user"]
--- services.py
+++ services.py
@@ -1,9 +1,32 @@
 from django.core.exceptions import ValidationError
 from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 def get_path_upload_avatar(instance, file):
     return f"avatar/{instance.id}/{file}"
+
+
+def get_path_upload_textfile(instance, file):
+    return f"textfile/{instance.id}/{file}"
+
+
+def get_path_upload_comments_images(instance, file):
+    return f"comments_images/{instance.id}/{file}"
+    
+
+def validate_avatar(image):
+    max_width = 600
+    max_height = 600
+    img = Image.open(image)
+
+
+    if img.width > max_width or img.height > max_height:
+        raise ValidationError(f"Изображение должно быть не более {max_width}x{max_height} пикселей.")
+
+    if img.format.lower() not in ['jpg', 'jpeg', 'png']:
+        raise ValidationError("Допустимые форматы изображений: JPG, JPEG, PNG")
 
 
 def validate_size_upload_textfile(file):
@@ -13,14 +36,20 @@ def validate_size_upload_textfile(file):
         raise ValidationError(f"Максимальный размер файла - {limit_upload_kb}килобайт")
     
 
-def validate_avatar(image):
-    max_width = 320
-    max_height = 240
+def validate_upload_comments_images(image):
+    max_height = 320
+    max_width = 240
+
     img = Image.open(image)
+    
+    if img.format.lower() not in ['jpg', 'jpeg', 'png']:
+        raise ValidationError("Допустимые форматы изображений: JPG, JPEG, PNG")
 
+    if img.height > max_height or img.width > max_width:
+        img.tumbnail((max_height, max_width))
+        
+        img_io = BytesIO()
+        img.save(img_io, format=img.format)
+        image = InMemoryUploadedFile(img_io, 'ImageField', image.name, image.content_type, img_io.getbuffer().nbytes, None)
 
-    if img.width > max_width or img.height > max_height:
-        raise ValidationError(f"Изображение должно быть не более {max_width}x{max_height} пикселей.")
-
-    if img.format.lower() not in ['jpg', 'jpeg', 'png', 'gif']:
-        raise ValidationError("Допустимые форматы изображений: JPG, JPEG, PNG, GIF.")
+    return image
